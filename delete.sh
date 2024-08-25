2eb42fb477966d1d0b041544816d8a75309c976e
Samandar Jumanazarov <samandarjs2001@gmail.com>
2024-08-25 16:25:49 +0000

    test: delete.sh

--- delete.sh
+++ delete.sh
@@ -4,6 +4,5 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 $PSQL "DELETE FROM properties WHERE atomic_number = 1000;"
 $PSQL "DELETE FROM elements WHERE atomic_number = 1000;"
 echo "Deleted element with atomic_number 1000 from the database."
- 
 $PSQL "ALTER TABLE properties DROP COLUMN type;"
 echo "Deleted column type from properties."
