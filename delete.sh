5b5b308a405a82e8f1bcf52306caa49bda2111af
Samandar Jumanazarov <samandarjs2001@gmail.com>
2024-08-25 16:12:25 +0000

    fix: delete.sh is fixed

--- delete.sh
+++ delete.sh
@@ -1,5 +1,4 @@
 # Delete the element with atomic_number 1000 from the 'properties' and 'elements' tables, if it exists.
 $PSQL "DELETE FROM properties WHERE atomic_number = 1000;"
 $PSQL "DELETE FROM elements WHERE atomic_number = 1000;"
-
 echo "Deleted element with atomic_number 1000 from the database."
