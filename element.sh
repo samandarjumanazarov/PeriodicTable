2625aa0c9863fab5c670e8395c1590fed60bd1f0
Samandar Jumanazarov <samandarjs2001@gmail.com>
2024-08-25 15:56:04 +0000

    fix: delete one element

--- element.sh
+++ element.sh
@@ -1,45 +1,9 @@
 # #!/bin/bash
-# PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+$PSQL "DELETE FROM properties WHERE atomic_number = 1000;"
+$PSQL "DELETE FROM elements WHERE atomic_number = 1000;"
 
-# # Check if an argument is provided
-# if [[ -z $1 ]]
-# then
-#   echo "Please provide an element as an argument."
-#   exit
-# fi
-
-# # Determine if the input is a number (atomic number) or a string (symbol or name)
-# if [[ $1 =~ ^[0-9]+$ ]]
-# then
-#   # Input is a number, assume atomic number
-#   ELEMENT_INFO=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type 
-#                         FROM elements 
-#                         INNER JOIN properties USING(atomic_number) 
-#                         INNER JOIN types USING(type_id) 
-#                         WHERE atomic_number = $1")
-# else
-#   # Input is a string, assume symbol or name
-#   ELEMENT_INFO=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type 
-#                         FROM elements 
-#                         INNER JOIN properties USING(atomic_number) 
-#                         INNER JOIN types USING(type_id) 
-#                         WHERE symbol = '$1' OR name = '$1'")
-# fi
-
-# # Check if the query found an element
-# if [[ -z $ELEMENT_INFO ]]
-# then
-#   echo "I could not find that element in the database."
-# else
-#   # Read query output into variables
-#   IFS="|" read ATOMIC_NUMBER SYMBOL NAME MASS MELTING_POINT BOILING_POINT TYPE <<< "$ELEMENT_INFO"
-  
-#   # Output the formatted element information
-#   echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-# fi
-#!/bin/bash
-#!/bin/bash
-#!/bin/bash
+echo "Deleted element with atomic_number 1000 from the database."
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # Check if an argument is provided
